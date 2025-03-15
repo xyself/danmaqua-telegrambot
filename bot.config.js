1a87f7cfd9dcc4f673907f494556dedcd34574de
xyself <133769218+xyself@users.noreply.github.com>
2025-03-15 13:50:53 +0800

    date

---
+++ bot.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    dataDir: './data',
+    dataSaveInterval: 10000,
+    logsDir: './data/logs/bot',
+    botToken: '',
+    botProxy: null,
+    botAdmins: [],
+    // B站登录Cookie中的SESSDATA值，用于获取弹幕用户名等信息
+    bilibiliSessData: '',
+    debugMode: false,
+    rateLimit: {
+        enabled: false,
+        redisServer: '127.0.0.1:6379',
+        selectDB: 1
+    },
+    statistics: {
+        enabled: false,
+        redisServer: '127.0.0.1:6379',
+        selectDB: 1
+    },
+    danmakuSources: [
+        {
+            id: 'bilibili',
+            description: '哔哩哔哩直播弹幕',
+            type: 'common-danmaku-ws',
+            value: {
+                url: 'localhost:8001',
+                basicAuth: 'testPassword'
+            }
+        },
+        {
+            id: 'douyu',
+            description: '斗鱼直播弹幕',
+            type: 'common-danmaku-ws',
+            value: 'localhost:8002'
+        },
+        {
+            id: 'local',
+            description: '本地测试弹幕服务器',
+            type: 'common-danmaku-ws',
+            value: 'localhost:8003',
+            enabled: false
+        }
+    ]
+};
